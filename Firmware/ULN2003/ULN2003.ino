82ce86e253a5f1828d0c63ec28ea164ad11a9205
Adam Haile <adammhaile@gmail.com>
2018-10-09 17:21:58 -0400

    Working for ATTinyX4

--- Firmware/ULN2003/ULN2003.ino
+++ Firmware/ULN2003/ULN2003.ino
@@ -6,28 +6,44 @@
 #define m3  3     // IN3 on the ULN2003 driver 1
 #define m4  4     // IN4 on the ULN2003 driver 1
 
-#define FWD 8
-#define REV 9
+#define FWD 7
+#define REV 8
 #define SPEED_IN A0
+#define STATUS 6
 
 #define STEPS_FWD 4096
 #define STEPS_REV -4096
 
-#define MAX_SPEED 2000
+#define MAX_SPEED 4000
 #define MIN_SPEED 500
+#define ACCEL     2000
 
-// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for
+// using the AccelStepper with 28BYJ-48
 AccelStepper motor(AccelStepper::HALF4WIRE , m1, m3, m2, m4);
+
+void startup() {
+  motor.enableOutputs();
+  motor.move(STEPS_FWD);
+  motor.runToPosition();
+  motor.move(STEPS_REV);
+  motor.runToPosition();
+}
 
 void setup() {
   pinMode(FWD, INPUT_PULLUP);
   pinMode(REV, INPUT_PULLUP);
   pinMode(SPEED_IN, INPUT);
+  pinMode(STATUS, OUTPUT);
 
   motor.disableOutputs();
-  motor.setMaxSpeed(2000.0);
-  motor.setAcceleration(4000.0);
+  motor.setMaxSpeed(MAX_SPEED);
+  motor.setAcceleration(ACCEL);
+
+  startup();
 }
+
+
 
 int _speed = 0;
 
@@ -35,14 +51,17 @@ void loop() {
   if(!digitalRead(FWD)){
     motor.enableOutputs();
     motor.move(STEPS_FWD);
+    digitalWrite(STATUS, HIGH);
     motor.run();
   }
   else if(!digitalRead(REV)){
     motor.enableOutputs();
     motor.move(STEPS_REV);
+    digitalWrite(STATUS, HIGH);
     motor.run();
   }
   else {
+    digitalWrite(STATUS, LOW);
     motor.setCurrentPosition(0);
     motor.disableOutputs();
     _speed = analogRead(SPEED_IN);
